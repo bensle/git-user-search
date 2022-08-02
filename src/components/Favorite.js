3fb1777d39466fb2a29ab4b35e8743b5c02daf50
Benjamin Scheffler <scheffler-benjamin@web.de>
2022-08-02 18:05:16 +0200

    Delete line

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -19,7 +19,6 @@ export default function Favorite({
 }
 
 const FavoriteAdded = styled(MdOutlineFavorite)`
-  /* background-color: transparent; */
   position: absolute;
   font-size: 2.2rem;
   color: red;
@@ -28,7 +27,6 @@ const FavoriteAdded = styled(MdOutlineFavorite)`
   padding: 5px;
 `;
 const AddFavorite = styled(MdOutlineFavoriteBorder)`
-  /* background-color: transparent; */
   position: absolute;
   font-size: 2.2rem;
   color: red;
